0470ad649cbee537dca373d96fcf1bdf20c5ba65
Federico De Sía <desiafederico@gmail.com>
2022-09-02 15:04:35 -0300

    Proyecto inicial

---
+++ frontend/src/main.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
+)
---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import { Center, Spinner } from "@chakra-ui/react"
+
+const HomePage = () => {
+    return <Center h='100vh'>
+        <Spinner/>
+    </Center>
+}
+
+export default HomePage
